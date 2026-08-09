3fd634a46aea8682b0ec1c2845dcd2740de55309
Moath Abdo <linktowindows00@gmail.com>
2026-08-09 17:08:33 +0300

    trust railway proxy for https asset urls

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/login');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
